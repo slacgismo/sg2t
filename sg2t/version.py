f720e524dcdedcc55e6fe1e18147f8c6f0af9bd1
David P. Chassin <dchassin@slac.stanford.edu>
2023-10-25 08:08:03 -0700

    Update version.py

--- sg2t/version.py
+++ sg2t/version.py
@@ -5,4 +5,4 @@ try:
     from setuptools_scm import get_version
     __version__ = get_version(root='..', relative_to=__file__)
 except Exception:
-    __version__ = '0.1.dev80+gdb4c198.d20230628'
+    __version__ = '0.1.dev77+gcab2a70.d20231025'
